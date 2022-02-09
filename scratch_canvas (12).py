b66108dba2c165037961a37faf849605d4c87aed
MuzainaBatool <95070742+MuzainaBatool@users.noreply.github.com>
2022-02-09 14:45:11 +0530

    Add files via upload

---
+++ scratch_canvas (12).py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+year=[2018,2019,2020,2021]
+population=[19.756,19.997,20.185,20.144]
+
+plt.bar(year,population)
+
+plt.xlabel('year')
+plt.ylabel('population')
+
+plt.title('population graph of mumbai')
+
+plt.show()
